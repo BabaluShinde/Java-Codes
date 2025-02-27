1b716f81355af706306e344bf4ae34ef15355610
Babalu <babalushinde1312@gmail.com>
2025-02-27 16:28:10 +0530

    Update in the code of this keyword and super keyword.

--- main/keywords/superKeyword.java
+++ main/keywords/superKeyword.java
@@ -1,7 +1,13 @@
 package main.keywords;
 
 class parent{
+	void show(){
+		System.out.println("Parent method");
+	}
 	
+	parent(){                   //constructor name must be same as the class name.(parent)
+		System.out.println("This the parent Constructor");
+	}
 	int a=10;
 	static int b=20;
 	
@@ -9,10 +15,15 @@ class parent{
 class child extends parent{
 	void test() {
 		
-		System.out.println(super.a);
+		System.out.println(super.a);  //Accessing parent class variable.
 		
 		System.out.println(super.b);
+		super.show();                 //Calling parent class method.
 
+	}
+	child(){						//constructor name must be same as the class name as the e.g. child with case sensitive.
+		super();                     //invoked parent class constructor i.e. parent();
+		System.out.println("This is chilld constructor.");
 	}
 }
 public class superKeyword {
--- main/keywords/thisKeyword.java
+++ main/keywords/thisKeyword.java
@@ -5,18 +5,42 @@ package main.keywords;
 public class thisKeyword {
 	int a=10;
 	static int b=20;
+	int age;
+	String name;
+	
+	thisKeyword(){
+		this("Babalu",23);
+		System.out.println("Constructor test.");
+		
+	}
+	
+	thisKeyword(String name, int age){
+		this.name =name;
+		this.age=age;
+	}
 	
 	void test() {
 		this.a=100;
 		System.out.println(a);
 		this.b=600;
 		System.out.println(b);
+		
+		
+		
+	}
+	
+	void display() {
+		System.out.println("Name:"+name +",age: "+age);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		new thisKeyword().test();
+		
+		
+		thisKeyword p1= new thisKeyword();
+		p1.display();
 
 	}
 
